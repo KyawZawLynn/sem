f9d51f3be2f267cfb188547adac59b2e2c704ad0
Kyaw Zaw Lynn <40437522@live.napier.ac.uk)>
2020-01-07 13:51:27 +0630

    Creating a Network Bridge in Docker

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,8 +9,10 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        //MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -27,4 +29,5 @@ public class App
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
     }
+
 }
