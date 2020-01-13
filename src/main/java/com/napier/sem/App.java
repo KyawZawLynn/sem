defe48b6145ce146970d8d5a6874e70d927e78f3
Kyaw Zaw Lynn <40437522@live.napier.ac.uk)>
2020-01-13 15:40:00 +0630

    Display Employee Information

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,7 +11,10 @@ public class App
 
         // Connect to database
         a.connect();
-
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
         // Disconnect from database
         a.disconnect();
     }
@@ -114,6 +117,21 @@ public class App
         }
     }
 
+    // display employee information
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
 
 
 }
